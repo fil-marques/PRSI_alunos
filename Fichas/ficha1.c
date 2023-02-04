221cefe4dc9ba1555e744ebb2afeb7d21d3532c4
Filipe Marques <filipe.marques.it@outlook.pt>
2023-02-04 20:44:02 +0000

    código inicial

---
+++ Fichas/ficha1.c
@@ -0,0 +1,24 @@
+/*
+    Ficha 1 - Noção de Variável Local utilizando a função Swap como exemplo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+void troca(int* x, int* y){
+    int aux;
+
+    aux = *x; //temporariamente guardamos x
+    *x = *y; //apagamos o que estava em x com o valor de y
+    *y = aux; //apagamos y com o valor que estava em x
+}
+
+int main(){
+    int a, b;
+
+    printf("Indique dois números: ");
+    scanf("%d %d", &a, &b);
+    troca(&a,&b);
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
