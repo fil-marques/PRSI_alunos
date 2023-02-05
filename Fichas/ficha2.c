75a6de34f117e49c5352d1d09615d433252fca3b
Filipe Marques <filipe.marques.it@outlook.pt>
2023-02-05 16:10:42 +0000

    representação ASCII

--- Fichas/ficha2.c
+++ Fichas/ficha2.c
@@ -8,10 +8,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
+void imprime_tesoura(){
+    printf(" _  __\n"
+        "(_)/ /\n"
+        "  / /\n" 
+        " / /_\n" 
+        "/_/(_)\n");
+}
+
+main(){
     srand(time(NULL)); /* Porquê o 'time' ? */
 
-    int res = rand() % 2; /* O que faz esta linha de código? */
+    int res = rand() % 3; /* O que faz esta linha de código? Quais os valores possíveis de 'res' */
 
     printf("\tResultado: %d\n", res); /* Que resultados poderão ocorrer aqui? */
 
@@ -20,5 +28,7 @@ int main(){
     /* Re-utilizando o código, recria o jogo Pedra, Papel, Tesoura numa função jogo() em
     separado */
 
-    return 0;
+    if(res == 2)
+        imprime_tesoura();
+
 }
