d9efd2163752b2a6ce1b0b2c8f84c1e473619028
Filipe Marques <filipe.marques.it@outlook.pt>
2023-02-16 19:25:45 +0000

    Divisão em fases PRIMM

--- Fichas/ficha1.c
+++ Fichas/ficha1.c
@@ -1,29 +1,33 @@
 /*
-    Ficha 1 - Cálculo de energia verde
+    Ficha 1 - Cálculo de Energia Verde
 
-    O que faz este programa?
+    1 - O que faz este programa?
+    2 - Executa o programa e testa a tua hipótese
+    3 - Comenta nas linhas indicadas o que faz cada instrução
+    4 - Modifica e melhora o teu programa para que utilize um procedimento calcula_quantidade_paineis()
+    5 - Cria um novo programa que calcula_area_paineis() (dica: tenta re-utilizar o que já fizeste)
 */
 
 #include <stdio.h>
 
-int main() {
+main() {
     int consumo_diario;
     int dias_ano;
     float potencia_painel;
     float quantidade_painel;
     float area_necessaria;
 
+    dias_ano = 365;
+
     printf("Digite o consumo diário de energia da sua casa (em kWh): ");
     scanf("%d", &consumo_diario);
-    printf("Digite o número de dias por ano que deseja suprir com a energia solar: ");
-    scanf("%d", &dias_ano);
     printf("Digite a potência de cada painel solar (em Watts): ");
     scanf("%f", &potencia_painel);
 
-    quantidade_painel = (consumo_diario * dias_ano * 1000) / (potencia_painel * 6);
-    area_necessaria = quantidade_painel * 1.75;
+    quantidade_painel = (consumo_diario * dias_ano) / (potencia_painel * 6); //6 horas de luz plena
+    // area_necessaria = quantidade_painel * 1.75; //assumindo cada painel tem uma área de 1.75
+    // ^^ Função a criar
 
-    printf("Você precisará de aproximadamente %.2f painéis solares (%.2f m²) para suprir a sua casa.\n", quantidade_painel, area_necessaria);
-
-    return 0;
+    printf("Você precisará de aproximadamente %.2f painéis solares para suprir a sua casa.\n", quantidade_painel);
+//    printf("Você precisará de aproximadamente %.2f m² para suprir a sua casa.\n", area_necessaria);
 }
